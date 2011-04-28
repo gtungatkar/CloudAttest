50a8b63c28ce40caaba431b84e1e2b5699eaeed9
Ameya Gholkar <ameya.moses@gmail.com>
2011-04-27 22:54:07 -0400

    Clique algorithm - Tested and Working for 4 X 4 matrix.

--- Portal/findclique.c
+++ Portal/findclique.c
@@ -1,71 +1,115 @@
+/*
+Clique Detection Algorithm
+Tested and Working as of 4/27/2011
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
 //#include "balance.h"
 //COMMON *common from balance.c
 //extern int channel_count;
-int channel_count = 5;
+int channel_count = 4;
 //int *CL,*cl_b;
 //int *malicious;
 int malicious[5];
 int CL[10][10],cl_b[10][10];
 int clcnt = 0,clbcnt = 0,malcnt=0;
-int graph[5][5];
+int graph[4][4] = {0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0} ; //with 4 cliques - Tested
+//int graph[4][4] = {0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0} ; //with 1 clique - Tested
+//int graph[4][4] = {0,0,1,1,0,0,1,0,1,1,0,1,1,0,1,0} ; // with 2 cliques - 1011, 0110 -Tested
+//int graph[4][4] = {0,1,0,0,1,0,1,1,0,1,0,1,0,1,1,0} ; // with 2 cliques - 1100, 0111 - Tested
+//int graph[4][4] = {0,1,1,0,1,0,1,0,1,1,0,1,0,0,1,0} ; // with 2 cliques - 1110, 0011 - Tested
 int f[5];
+int N[4];
 
 
+void initialize_array(int a[])
+{
+    int i;
+ for(i = 0;i<channel_count;i++)
+    a[i] = 0;
 
-void find_common_elements(int N1[], int N2[], int new[])
+}
+
+void intersection_elements(int N1[], int N2[], int new[])
 {
         int i,new_cnt = 0;
         for(i = 0;i < channel_count;i ++)
         {
                 if(N1[i] == N2[i]) //found intersection between N1 and N2
-                        new[i] = i;
+                {
+                    if(N1[i]!=0)
+                    {
+                        new[i] = 1;
+                    }
+                    else
+                    {
+                        new[i] = 0;
+                    }
+                }
+
+                else
+                        new[i] = 0;
         }
-}
+} //Tested
 
 
-void subtract_element(int P[], int i)
+int subtract_element(int P[], int i)
 {
 
-        P[i] = -1;
+    if(P[i] != 0)
+    {
+        P[i] = 0;
+        return 1;
+    }
+    else
+        return 0;
 
-}
 
-void union_element(int R[], int i)
+} //Tested
+
+int XUnion_element(int R[], int i)
 {
-        if(R[i] == -1)
+        if(R[i] == 0)
         {
-                R[i] = i;
-		printf("\n Unioning!");
+                R[i] = 1;
+                printf("\n Unioning!");
+                return 1;
         }
+        else    return 0;
 }
 
 
-void union_Relement(int R[], int i,int Rnew[])
+void union_element(int R[], int Rnew[], int i)
 {
 	int j,ncnt = 0;
-	for(j=0;j<5;j++)
-	  if(R[j] != -1)
-          {
-                Rnew[ncnt++] = R[j];
-                
-          }
-	Rnew[ncnt]  = i;
+	for(j=0;j<channel_count;j++)
+	 {
+	    if(j == i)
+	    {
+	        Rnew[ncnt++] = 1;
+	    }
+        else
+        {
+            Rnew[ncnt++] = R[j];
+        }
+
+
+	 }
+	//Rnew[ncnt]  = i;
 	printf("\nUnioning R , i to Rnew");
-}
+} //Tested
 
 
 int is_empty(int P[]){
         int i;
-        for(i=0;i<20;i++)
+        for(i=0;i<channel_count;i++)
         {
-                if(P[i]==-1)
-                        continue;
-                else
-                        return 0;
+                if(P[i] != 0)
+                    return 0;
         }
         return 1;
-}
+} //Tested
 
 
 
@@ -81,32 +125,20 @@ int is_neighbour(int i,int j){
 void find_neighbor_set(int curr, int N[])
 {
         int i,ncnt = 0;
-        for(i = 0; i<channel_count ; i++)
+        for(i=0; i<channel_count; i++)
         {
-                if(i!=curr)
-                {
-                        if(graph[i][curr] != 0 && graph[curr][i] != 0)  //putting neighbors of curr in N[]
-                        {
-                                N[i] = i;
-			//	printf("\n\tHERE");
-                        }
-                        else{
-                                N[i] = -1;
-                        }
-                }
-                else{
-                        N[i] = -1;
+            if(graph[curr][i] == 1)
+            {
+                N[i] = 1;
+            }
+            else
+            {
+                N[i] = 0;
+            }
 
-                }
         }
 
-/*	            printf("\n\n (IN FUNCTION) Neighbor Set of: %d \n ",curr);
-                        for(i=0;i<5;i++)
-				if(N[i]!=-1)
-                                printf("\t %d;",N[i]);*/
-
-}
-
+} // Tested
 
 
 unsigned char find_node_in_clique(int node_j, int clique[][10])
@@ -132,13 +164,13 @@ int sizeofR(int R[])
 {
 int i=0, count=0;
         for(i=0; i<channel_count; i++) {
-                if(R[i] != -1 ){
+                if(R[i] != 0 ){
                         count++;
                 }
         }
 return count;
 }
-
+/*
 void FindConsistencyClique(int R[], int P[], int X[] );
 void find_clique()
 {
@@ -150,8 +182,8 @@ void find_clique()
 //	cl_b=(int*)malloc(channel_count*sizeof(int));
 	//malicious=(int)malloc(channel_count*sizeof(int));
 //	CL=(int*)malloc(channel_count*channel_count*sizeof(int));
-	
-	
+
+
         graph[0][0] = 1;
 	graph[0][1] = 0;graph[1][0] = 0;
 	graph[0][2] = 1;graph[2][0] = 1;
@@ -169,11 +201,11 @@ void find_clique()
 
 	graph[3][3] = 1;
 	graph[3][4] = 1;graph[4][3] = 1;
-	
+
 	graph[4][4] = 1;
-	
-	
-	
+
+
+
 	for(i=0;i<5;i++)
 		f[i] = 0;
 
@@ -194,8 +226,8 @@ void find_clique()
 		for(j=0;j<5;j++){
 			if(CL[i][j]!=-1)
 				printf("\t %d",CL[i][j]);
-			
-		}	
+
+		}
 		printf("\n");
 	}
 	//Now, once we have the cliques, we need to find malicious nodes ...
@@ -211,11 +243,11 @@ void find_clique()
 		if(j > channel_count/2) // this is valid clique
 		{
 			cl_b[clbcnt][j] = i; //only this i'th clique will be used later
-		}	
+		}
 		clbcnt++;
 	}
 
-	
+
 
         printf("\n The cliques B  are: (CNT: %d)\n",clcnt);
         for(i=0;i<clbcnt;i++){
@@ -228,8 +260,8 @@ void find_clique()
         }
 
 
-	
-	
+
+
 	for(i = 0; i < channel_count ; i++){
 		if(!find_node_in_clique(i,cl_b))
 		{
@@ -240,7 +272,7 @@ void find_clique()
 		{
 			printf("\n %d is in cl_b!!",i);
 		}
-	
+
 		//if only 1 maximal clique in cl_b --- ATTACK MODELS FINDING --- NEEDED ????
 			//if number of cliques == 1 , nodes in this clique  => N_i
 				//if weights of edges from nodes in N_i to rest of nodes NOT in N_i are all 0s
@@ -270,80 +302,133 @@ int pivot(int P[])
 	}
 	return -1;
 }
+*/
 
-void FindConsistencyClique(int R[], int P[], int X[] ){
-	
+void print(int a[])
+{
+    int i=0;
+    for(i=0; i<4; i++)
+        printf("%d ",a[i]);
+
+    printf("\n");
+}
+
+int find_next(int P[])
+{
+        int i = 0;
+        for(i=0; i<channel_count; i++)
+        {
+                if(P[i] == 1)
+                {
+                    return i;
+                }
+        }
+        return -1;
+}
+
+
+void FindConsistencyClique(int R[], int P[], int X[], int curr ){
+
 
 	int i = 0,j,K;
+	int current = curr+1;
+	printf("\nNew CALL:  ------------------------------------\n");
+	printf("Elements in R : ");
+	print(R);
+    printf("Elements in P : ");
+	print(P);
+    printf("Elements in X : ");
+	print(X);
 	//int *Pnew,*Xnew,*N;
-	int Pnew[5],Xnew[5],N[5],Rnew[5];
-  	printf("\n New CALL: Size of: R: %d ; P: %d ; X: %d ",sizeofR(R),sizeofR(P),sizeofR(X));
+    printf("\n Size of: R: %d ; P: %d ; X: %d ",sizeofR(R),sizeofR(P),sizeofR(X));
+
+
+
 	if( is_empty(P) && is_empty(X) && (sizeofR(R)>1) )
 	{
 		//Report R as Maximal Clique
-		printf("\nForming Clique");
-		for(i=0 ; i<channel_count ; i++)
-		{	
-			if(R[i]!= -1)
-				CL[clcnt][i] = R[i];
+		int j;
+		printf("\nInside New call - Forming Clique -------------------------------------------------Current = %d i = %d\n",current,i);
+		printf("\nWeGotClique!!!!!!!!!!!!!!!: ");
+		for(j=0 ; j<channel_count ; j++)
+		{
+			if(R[j]!= 0){
+				CL[clcnt][j] = R[j];
+
+			}
+			printf("\t %d",CL[clcnt][j]);
 		}
+        //initialize_array(R);
 		clcnt++;
+
 
 	}
 	else
 	{
 		//Select Pivot Node K
-		K = pivot(P);
-		printf("\n\t\t Got  Pivot: %d",K);
-		if(K == -1) // error in finding pivot
+		//K = pivot(P);
+		//printf("\n\t\t Got  Pivot: %d",K);
+		/*if(K == -1) // error in finding pivot
 		{
 			printf("\nreturning as K = -1");
 			return;
-		}
-		for(i=0; i<channel_count; i++)
+		}*/
+		while((i=find_next(P)) != -1)//for(i = current; i<channel_count; i++)
 		{
+		    printf("\nInside For Loop ------------------------------------Current = %d \n",current);
+		    printf("i = %d \n",i);
 			//Pnew=(int)malloc(channel_count*sizeof(int));
 			//Xnew=(int)malloc(channel_count*sizeof(int));
 			//N=(int)malloc(channel_count*sizeof(int));
-		 	for(j = 0;j < channel_count;j ++)
-	 	        {
-            			Pnew[j]=Xnew[j]=N[j]=Rnew[j]=-1;
-        		}
-			
-			
 
-			if(K!=P[i] && P[i]!=-1){
-			if(is_neighbour(K,i)==0)
-			{
+			//if(K!=P[i] && P[i]!=-1){
+			//if(is_neighbour(K,i)==0)
+			int Pnew[4],Xnew[4],Rnew[4];
+               // printf("\n----------------------------Elements in P : ");
+               // print(P);
+                initialize_array(Rnew);
+                initialize_array(Pnew);
+                initialize_array(Xnew);
+                //printf("\n----------------------------Elements in P : ");
+                //print(P);
 				 find_neighbor_set(i,N);
 
-			         printf("\n\n Neighbor Set of: %d \n ",i);
-                        	 for(j=0;j<5;j++)
-                                	if(N[j]!=-1)
-                                  		printf("\t %d;",N[j]);
 
+
+
+              //  if(sizeofR(P) != 0)
+              //  {
 				subtract_element(P,i);
 				printf("\n\t : After Subtraction of %d from P , size of P is: %d",i,sizeofR(P));
-	
+                printf("Elements in P: ");
+                print(P);
 				//going according to the algorithm... adding Rnew also
-				union_Relement(R,i,Rnew);
+				union_element(R,Rnew,i);
 
-				printf("\n\t : After Union of R , size of R is: %d \t",sizeofR(R));
-				for (j =0 ; j<5;j++)
-					printf("R[%d]: %d  ",j,R[j]);
-				find_common_elements(P,N,Pnew);
-				find_common_elements(X,N,Xnew);
-				printf("\n recursion: Size of: R: %d ; Pnew: %d ; Xnew: %d ; N[%d]:%d",sizeofR(R),sizeofR(Pnew),sizeofR(Xnew),i,sizeofR(N));
-				FindConsistencyClique(Rnew, Pnew, Xnew);
-				union_element(X,i);	
-				printf("\n\t : After Union of X , size of X is: %d \t",sizeofR(X));
-			}
-			}
-
+				printf("\n\t : After Union of R , size of R is: %d \n Elements in Rnew: ",sizeofR(Rnew));
+                print(Rnew);
+                printf("\n\n Neighbor Set of: %d \n ",i);
+                print(N);
+				intersection_elements(P,N,Pnew);
+				printf("Pnew: ");
+				print(Pnew);
+                intersection_elements(X,N,Xnew);
+				printf("Xnew: ");
+				print(Xnew);
+				//copy_array(X)
+				printf("\n recursion: Size of: Rnew: %d ; Pnew: %d ; Xnew: %d ; N[%d]:%d",sizeofR(Rnew),sizeofR(Pnew),sizeofR(Xnew),i,sizeofR(N));
+				FindConsistencyClique(Rnew, Pnew, Xnew,i);
+				union_element(X,X,i);
+				printf("\n\t : After UnionofX , size of X is: %d \n After Union of X : ",sizeofR(X));
+                print(X);
+                //current++;
+                //initialize_array(R);
+             //   }
 		}
 		//return;
 	}
 	printf("\n returning :( ");
+	printf("\nAfter The Union -------------------------------------------------Current = %d i = %d\n",current,i);
 //	return;
 }
 
@@ -356,11 +441,30 @@ int display(){
 int main()
 {
 	int i=0;
-	find_clique();
+	/*find_clique();
 	printf("\n\n Malicious!! X-(: \n ");
-	for(i=0;i<malcnt;i++)
-	{
-		if(malicious[i]!=-1)
-		printf("\t %d;",malicious[i]);			
-	}
+//	for(i=0;i<malcnt;i++)
+//	{
+//		if(malicious[i]!=-1)
+//		printf("\t %d;",malicious[i]);
+//	}*/
+	int N1[4] = {1,0,0,0};
+	int N2[4] = {1,0,0,1};
+	int N3[4] = {0};
+    int R[4] = {0};
+    int P[4] = {1,1,1,1};
+    int X[4] ={0};
+	//intersection_elements(N1, N2, N3);
+	//subtract_element(N1,0);
+	//XUnion_element(N1,3);
+    //union_Relement(N1,N3,0);
+    //printf("Empty? : %d\n",is_empty(N1));
+    //printf("Size: %d",sizeofR(N3));
+    //find_neighbor_set(3,N3);
+    FindConsistencyClique(R,P,X,-1);
+		for(i=0;i<channel_count;i++)
+		{
+		//if(malicious[i]!=-1)
+		printf("\t %d;",N3[i]);
+        }
 }
